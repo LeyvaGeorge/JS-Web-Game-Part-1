0651d5e1d909ee0954a0a6e23e4bbb6a5cb53285
George Leyva <leyvageorge@hotmail.com>
2023-01-24 13:14:33 -0800

    updated to new repository info callback-Practice

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    document.body.append(image)
+    return image
 }
+
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
+
+
+let greenCharacter = newImage('assets/green-character.gif')
+move(greenCharacter).to(100, 250)
+
+
+
+newImage('assets/tree.png', 200, 450)
+newImage('assets/pillar.png', 350, 250)
+newImage('assets/pine-tree.png', 450, 350)
+newImage('assets/crate.png', 150, 350)
+newImage('assets/well.png', 500, 575)
+
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
-    item.addEventListener('click', function(){
+    item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
-        inventoryItem.src = url
+        inventoryItem.src = url;
         inventory.append(inventoryItem)
     })
+    return item
 }
 
+newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
 
-let inventory = document.createElement('div')
 function newInventory(){
-    
+    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -34,16 +60,7 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
-newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pineTree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+const inventory = newInventory()
