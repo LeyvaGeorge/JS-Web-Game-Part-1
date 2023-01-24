d9aa42baabe9a06554304a0a71844f5447f2bdf2
George Leyva <leyvageorge@hotmail.com>
2023-01-24 14:48:03 -0800

    updated info

--- index.js
+++ index.js
@@ -1,66 +1,34 @@
-function newImage(url){
-    let image = document.createElement('img')
-    image.src = url
-    document.body.append(image)
-    return image
-}
-
-function move(image){
-    image.style.position = 'fixed'
-    
-    function moveToCoordinates(left, bottom){
-        image.style.left = left + 'px'
-        image.style.bottom = bottom + 'px'
-    }
-
-    return {
-        to: moveToCoordinates
-    }
-}
-
-
-let greenCharacter = newImage('assets/green-character.gif')
-move(greenCharacter).to(100, 250)
-
-
-
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-    item.addEventListener('click', () => {
-        item.remove()
-        let inventoryItem = document.createElement('img')
-        inventoryItem.src = url;
-        inventory.append(inventoryItem)
-    })
-    return item
-}
-
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
-
-function newInventory(){
-    let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
-    return inventory
-}
-
 const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+
+if(direction === 'west'){
+    x = x - 1
+}
+if(direction === 'north'){
+    y = y + 1
+}
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
+    y = y - 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+
+setInterval(moveCharacter, 1)                                    
+
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
