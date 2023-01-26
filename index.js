7bd9c56eebebc0313cd7dbc0930a4c87adc29595
George Leyva <leyvageorge@hotmail.com>
2023-01-25 16:58:34 -0800

    changes to code fit more like asked for

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-
-function handleDirectionChange(direction){
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
+function newItem(source, left, bottom){
+    let item = document.createElement('img')    //create an image to pace onto the website
+    item.src = source               //source of img
+    item.style.position = 'fixed'   //object doesn't have animation
+    item.style.left = left + 'px'  //moves item from left to right
+    item.style.bottom = bottom + 'px'   //moves item from bottom of screen higher
+    document.body.append(item)      //add item to the website
+    return item
 }
-move(character).withArrowKeys(100, 250, handleDirectionChange)
+function clickableItem(source, left, bottom){
+    let object = newItem(source, left, bottom)
+          
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pineTree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+}
+newItem('assets/green-character.gif', 100, 100)
+newItem('assets/tree.png', 200, 300)
+newItem('assets/pillar.png', 350, 100)
+newItem('assets/crate.png', 150, 200)
+newItem('assets/well.png', 500, 425)
+newItem('assets/green-character.gif', 100, 100)
+
+clickableItem('assets/sword.png', 500, 405)
+clickableItem('assets/shield.png', 165, 185)
+clickableItem('assets/staff.png', 600, 100)
+
+
