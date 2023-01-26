a420622ec18ab25614bc78f74993e637f6290175
George Leyva <leyvageorge@hotmail.com>
2023-01-25 16:59:32 -0800

    changes modified

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ function newItem(source, left, bottom){
     return item
 }
 function clickableItem(source, left, bottom){
-    let object = newItem(source, left, bottom)
-          
+    let object = newItem(source, left, bottom)  //adds img to website
+//double click function to remove img          
     object.addEventListener('dblclick', () => {
         object.remove()
     })
 
 }
+//==============creates IMG====================
 newItem('assets/green-character.gif', 100, 100)
 newItem('assets/tree.png', 200, 300)
 newItem('assets/pillar.png', 350, 100)
 newItem('assets/crate.png', 150, 200)
 newItem('assets/well.png', 500, 425)
 newItem('assets/green-character.gif', 100, 100)
-
+//================Clickable IMG================
 clickableItem('assets/sword.png', 500, 405)
 clickableItem('assets/shield.png', 165, 185)
 clickableItem('assets/staff.png', 600, 100)
